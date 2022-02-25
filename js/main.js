86b0682060f3d2e322f713921009e18991375ced
Faizal Khalifa <faizalfkhalifa786@gmail.com>
2022-02-25 10:42:38 +0530

    Sign Up and LogIn Page is add

--- js/main.js
+++ js/main.js
@@ -9,7 +9,11 @@ const right_arrow = document.getElementById("right-arrow");
 const sliders = document.querySelectorAll(".our-clients-massage-containers");
 let curSlide = 0;
 const log_in = document.getElementById("log-in");
-const show_password = document.getElementById("show-password");
+const show_password_log_in = document.getElementById("show-password-log-in");
+const sign_up_btn = document.getElementById("sign-up-btn");
+const sign_in_btn = document.getElementById("sign-in-btn");
+const sign_in = document.getElementById("sign-in");
+const show_password_sign_up = document.getElementById("show-password-sign-up");
 
 // scroll
 window.addEventListener("scroll", () => {
@@ -54,11 +58,12 @@ if (img_slider) {
     5000
   );
 }
+
 // clients massage slider
 const goToSlides = function (slide) {
   sliders.forEach(function (el, i) {
     el.style.transform = `translateX(${100 * (i - slide)}%)`;
-    el.style.transition = "transform 2s ease-in-out";
+    el.style.transition = "transform 1s ease-in-out";
   });
 };
 
@@ -78,6 +83,7 @@ const pevPic = function () {
   curSlide--;
   goToSlides(curSlide);
 };
+
 goToSlides(curSlide);
 
 if (left_arrow && right_arrow) {
@@ -85,45 +91,66 @@ if (left_arrow && right_arrow) {
   right_arrow.addEventListener("click", nextPic);
 }
 
-// log in validations
-if (show_password) {
-  show_password.checked = false;
-}
+// lon in and sigin up
 
-const showPassword = function () {
-  const user_password = document.getElementById("user-password");
-  user_password.type = show_password.checked ? "text" : "password";
+const checkFrom = function () {
+  const form_section = document.querySelectorAll(".form-section");
+  form_section.forEach(function (el) {
+    if (el.classList.contains("hide-form")) {
+      el.classList.remove("hide-form");
+    } else {
+      el.classList.add("hide-form");
+    }
+  });
 };
 
-const formValidation = function (e) {
-  const user_name = document.getElementById("user-name");
-  const user_password = document.getElementById("user-password");
+if (sign_up_btn && sign_in_btn) {
+  sign_up_btn.addEventListener("click", checkFrom);
+  sign_in_btn.addEventListener("click", checkFrom);
+}
 
-  const createErrorMassage = function (currElement, massage) {
-    const span = document.createElement("div");
+// log in validations
+const checkCheck = function (show_password) {
+  show_password.checked = false;
+};
 
-    span.classList.add("massage-error");
+if (show_password_log_in) {
+  checkCheck(show_password_log_in);
+}
 
-    span.innerHTML = "&#128712;".concat(" ").concat(massage);
+if (show_password_sign_up) {
+  checkCheck(show_password_sign_up);
+}
 
-    currElement.insertAdjacentHTML("afterend", span.outerHTML);
-  };
+const createErrorMassage = function (currElement, massage) {
+  const span = document.createElement("div");
+  span.classList.add("massage-error");
+  span.innerHTML = "&#128712;".concat(" ").concat(massage);
+  currElement.insertAdjacentHTML("afterend", span.outerHTML);
+};
+
+const logInFormValidation = function (e) {
+  const log_in_user_name = document.getElementById("log-in-user-name");
+  const log_in_user_password = document.getElementById("log-in-user-password");
 
   const massage_error = document.querySelectorAll(".massage-error");
   massage_error.forEach((el) => el.remove());
 
-  if (!user_name.value) {
-    createErrorMassage(user_name, "Enter a user name");
-  } else if (user_name.value.length < 10) {
-    createErrorMassage(user_name, "user name must be at least 10 characters");
+  if (!log_in_user_name.value) {
+    createErrorMassage(log_in_user_name, "Enter a user name");
+  } else if (log_in_user_name.value.length < 6) {
+    createErrorMassage(
+      log_in_user_name,
+      "user name must be at least 6 characters"
+    );
   }
 
-  if (!user_password.value) {
-    createErrorMassage(user_password, "Enter a password");
-  } else if (user_password.value.length < 10) {
+  if (!log_in_user_password.value) {
+    createErrorMassage(log_in_user_password, "Enter a password");
+  } else if (log_in_user_password.value.length < 6) {
     createErrorMassage(
-      user_password,
-      "password must be at least 10 characters"
+      log_in_user_password,
+      "password must be at least 6 characters"
     );
   }
 
@@ -131,7 +158,67 @@ const formValidation = function (e) {
     e.preventDefault();
 };
 
-if (show_password && log_in) {
-  show_password.addEventListener("click", showPassword);
-  log_in.addEventListener("submit", formValidation);
+const signInFormValidation = function (e) {
+  const sign_in_full_name = document.getElementById("sign-in-full-name");
+  const sign_in_user_name = document.getElementById("sign-in-user-name");
+  const sign_in_password = document.getElementById("sign-in-user-password");
+  const sign_in_re_password = document.getElementById(
+    "sign-in-user-re-password"
+  );
+
+  const massage_error = document.querySelectorAll(".massage-error");
+  massage_error.forEach((el) => el.remove());
+
+  if (!sign_in_full_name.value) {
+    createErrorMassage(sign_in_full_name, "Enter a Full name");
+  } else if (sign_in_full_name.value.length < 5) {
+    createErrorMassage(sign_in_full_name, "Please Enter a full name");
+  }
+
+  if (!sign_in_user_name.value) {
+    createErrorMassage(sign_in_user_name, "Enter a user name");
+  } else if (sign_in_user_name.value.length < 6) {
+    createErrorMassage(
+      sign_in_user_name,
+      "user name must be at least 6 characters"
+    );
+  }
+
+  if (!sign_in_password.value) {
+    createErrorMassage(sign_in_password, "Enter a password");
+  } else if (sign_in_password.value.length < 6) {
+    createErrorMassage(
+      sign_in_password,
+      "password name must be at least 6 characters"
+    );
+  }
+
+  if (!sign_in_re_password.value) {
+    createErrorMassage(sign_in_re_password, "Enter a password");
+  } else if (sign_in_password.value !== sign_in_re_password.value) {
+    createErrorMassage(sign_in_re_password, "password is not same");
+  }
+
+  if (document.querySelectorAll(".massage-error").length > 0)
+    e.preventDefault();
+};
+
+if (sign_in && show_password_sign_up) {
+  show_password_sign_up.addEventListener("click", function () {
+    const user_password = document.getElementById("sign-in-user-password");
+    const user_re_password = document.getElementById(
+      "sign-in-user-re-password"
+    );
+    user_password.type = show_password_sign_up.checked ? "text" : "password";
+    user_re_password.type = show_password_sign_up.checked ? "text" : "password";
+  });
+  sign_in.addEventListener("submit", signInFormValidation);
+}
+
+if (show_password_log_in && log_in) {
+  show_password_log_in.addEventListener("click", function () {
+    const user_password = document.getElementById("log-in-user-password");
+    user_password.type = show_password_log_in.checked ? "text" : "password";
+  });
+  log_in.addEventListener("submit", logInFormValidation);
 }
